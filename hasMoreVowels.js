56ca23549dbc5647e9b1e013a9c48f89292ed0e2
Damien Redd <damiens.redd@gmail.com>
2023-02-06 09:44:00 -0600

    discussion

--- hasMoreVowels.js
+++ hasMoreVowels.js
@@ -1,12 +1,30 @@
-const hasMoreVowels = (str) =>{
-    vowelsCount = 0
-    constCount = 0
-    const vowels = ['a', 'e', 'i', 'o', 'u']
-    for (let char of str) {
-        if (vowels.includes(char)) {
-            vowelsCount++
-        } else constCount++
+// const hasMoreVowels = (str) =>{
+//     vowelsCount = 0
+//     constCount = 0
+//     const vowels = ['a', 'e', 'i', 'o', 'u']
+//     for (let char of str) {
+//         if (vowels.includes(char)) {
+//             vowelsCount++
+//         } else constCount++
+//     }
+// return `The number of vowels is ${vowelsCount}, and the number of consonants is ${constCount}.`
+// }
+// console.log(hasMoreVowels('quetsylsakatanengo'))
+
+console.log('===============================')
+
+const hasMoreVowels = word => {
+    word = word.toLowerCase()
+    let vowels = ['a','e','i','o','u']
+    let vowelCount = 0
+    for (i = 0; i < word.length; i++){
+        if (vowels.includes(word[i]) ){
+            vowelCount++
+        }
     }
-return `The number of vowels is ${vowelsCount}, and the number of consonants is ${constCount}.`
+    // if (str.length / 2 <= vowelCount){
+    //     return false
+    
+    return  word.length/2 < vowelCount
 }
-console.log(hasMoreVowels('quetsylsakatanengo'))
+console.log(hasMoreVowels('Moose'))
